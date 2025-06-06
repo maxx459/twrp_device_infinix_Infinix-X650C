84055d86e919d7c3900e9f7a14beddaf42e1a6e5
Bitra Somanath charan <124593799+maxx459@users.noreply.github.com>
2025-06-07 01:38:04 +0530

    x650c: update device configurations

--- omni_Infinix-X650C.mk
+++ omni_Infinix-X650C.mk
@@ -12,18 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from Infinix-X650C device
-$(call inherit-product, device/infinix/Infinix-X650C/device.mk)
-
 PRODUCT_DEVICE := Infinix-X650C
 PRODUCT_NAME := omni_Infinix-X650C
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X650C
 PRODUCT_MANUFACTURER := infinix
-
-PRODUCT_GMS_CLIENTID_BASE := android-transsion-infinix-rev1
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="full_x650c_h626-user 9 PPR1.180610.011 111759 release-keys"
-
-BUILD_FINGERPRINT := Infinix/H626/Infinix-X650C:9/PPR1.180610.011/DEJ-210525V327:user/release-keys
